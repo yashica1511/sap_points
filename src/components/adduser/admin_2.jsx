feac0e79a765e1fb12841d17b05863c366721aac
Sanjay <sanjayr.22aid@kongu.edu>
2024-05-12 18:34:30 +0530

    changes to overflow in staff admin view

--- src/components/adduser/admin_2.jsx
+++ src/components/adduser/admin_2.jsx
@@ -4,66 +4,54 @@ import supabase from '../../database/Client';
 import { useNavigate } from 'react-router-dom';
 
 function Admin_2() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    async function fetchData() {
-        const { data: staff_detils, error } = await supabase.from('staff_detils').select('*');
+  async function fetchData() {
+    const { data: staff_detils, error } = await supabase.from('staff_detils').select('*');
 
-        if (error) {
-            console.error('Error fetching data:', error);
-        } else {
-            setData(staff_detils);
-            console.log(staff_detils);
-        }
+    if (error) {
+      console.error('Error fetching data:', error);
+    } else {
+      setData(staff_detils);
+      console.log(staff_detils);
     }
+  }
 
-    return (
-        <>
-            <MainPage />
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[1279px] h-[645px] bg-[rgba(255,255,255,0.85)] rounded-xl">
-                <div className='w-[57.096px] h-[59.093px] bg-[url(../../images/logo.png)] bg-cover bg-no-repeat rounded-[6px] relative mt-[61.991px] mr-0 mb-0 ml-[609.099px]' />
-                <span className="block h-[70px] font-['Inknut_Antiqua'] text-[30px] font-normal leading-[39px] text-[#000] relative text-left rotate-[0.09deg] whitespace-nowrap z-[5] mt-[67.78px] mr-0 mb-0 ml-[350.863px]">
-                    DEPARTMENT OF ARTIFICIAL INTELLIGENCE
-                </span>
-                <table className='w-full overflow-auto'>
-  <div className='w-[320px] h-[48px] bg-[#fafafb] relative z-[1] mt-0 mr-0 mb-0 ml-0'>
-    <div className='w-[12.95%] h-full bg-[#fafafb] border-solid border border-[#dee1e6] absolute top-0 left-0 overflow-hidden z-[13]'>
-      <th className="flex w-[106px] h-[22px] justify-center items-start font-['Epilogue'] text-[14px] font-semibold leading-[22px] text-[#171a1f] absolute top-[calc(50%-11px)] left-[calc(50%-53px)] text-center whitespace-nowrap z-[14]">
-        S. No.
-      </th>
-    </div>
-    <div className='w-[52.47%] h-full bg-[#fafafb] border-solid border border-[#dee1e6] absolute top-0 left-[12.95%] overflow-hidden z-[12]'>
-      <th className="flex w-[512px] h-[22px] justify-center items-start font-['Epilogue'] text-[14px] font-semibold leading-[22px] text-[#171a1f] absolute top-[calc(50%-11px)] left-[calc(50%-256px)] text-center whitespace-nowrap z-[13]">
-        Name
-      </th>
-    </div>
-    <div className='w-[34.58%] h-full bg-[#fafafb] border-solid border border-[#dee1e6] absolute top-0 left-[65.42%] overflow-hidden z-[15] pointer'>
-      <th className="flex w-[336.44px] h-[22px] justify-center items-start font-['Epilogue'] text-[14px] font-semibold leading-[22px] text-[#171a1f] absolute top-[calc(50%-11px)] left-[calc(50%-168.5px)] text-center whitespace-nowrap z-[16]">
-        Email
-      </th>
-    </div>
-  </div>
-  {data.map((item, index) => (
-    <tr key={index} className='w-full h-[48px] bg-[rgba(0,0,0,0)] relative z-[2] mt-0 mr-0 mb-0 ml-0'>
-      <td className='w-[12.95%] h-full bg-[#fff] border-solid border border-[#dee1e6]'>
-        <span>{index + 1}</span>
-      </td>
-      <td className='w-[52.47%] h-full bg-[#fff] border-solid border border-[#dee1e6]'>
-        <span>{item.Name}</span>
-      </td>
-      <td className='w-[34.58%] h-full bg-[#fff] border-solid border border-[#dee1e6]'>
-        <span>{item.email}</span>
-      </td>
-    </tr>
-  ))}
-</table>
-</div>
-            </>
-    );
+  return (
+    <>
+
+      <div className='flex flex-col items-center mt-[100px]'>
+        <div className='h-[3vh] w-[3wv] bg-[url(../../images/logo.png)] bg-cover bg-no-repeat rounded-[6px]' />
+        <div className='flex flex-col items-center bg-tilebox bg-white rounded-lg h-[50vh] w-[60vh] overflow-y-auto p-2'>
+          <span className="block h-[70px] font-['Inknut_Antiqua'] text-[30px] font-normal  text-[#000] relative text-left rotate-[0.09deg] whitespace-nowrap z-[5]"> DEPARTMENT OF ARTIFICIAL INTELLIGENCE </span>
+          <div className='flex flex-col justify-center w-full'>
+            <table className='border border-1 overflow-y-auto rounded-lg'>
+              <thead>
+                <tr>
+                  <th className='px-4 py-2 border'>SNo</th>
+                  <th className='px-4 py-2 border'>Name</th>
+                  <th className='px-4 py-2'>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    <td className='border px-4 py-2'>{index + 1}</td>
+                    <td className='border px-4 py-2'>{item.Name}</td>
+                    <td className='border px-4 py-2'>{item.email}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Admin_2;
